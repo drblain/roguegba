5f2287445ed009d8831b4ab2d3a55cbac67afe7f
drblain <drblain22@gmail.com>
2025-09-27 10:47:42 -0400

    Initial cmake attempt

--- source/main.c
+++ source/main.c
@@ -9,33 +9,11 @@ typedef struct
 int main(void) {
     irq_init(NULL);
     irq_add(II_VBLANK, NULL);
-    REG_DISPCNT= DCNT_MODE3 | DCNT_BG2;
-
-    Pos player;
-    player.x = SCREEN_WIDTH / 2;
-    player.y = SCREEN_HEIGHT / 2;
-
-    M3_CLEAR();
+    REG_DISPCNT= DCNT_MODE5 | DCNT_BG2;
 
     while(1)
     {
         VBlankIntrWait();
-        m3_plot(player.x, player.y, CLR_BLACK);
-        key_poll();
-        
-        if (key_held(KEY_LEFT))
-            player.x = (player.x + SCREEN_WIDTH - 1) % SCREEN_WIDTH;
-
-        if (key_held(KEY_RIGHT))
-            player.x = (player.x + 1) % SCREEN_WIDTH;
-
-        if (key_held(KEY_UP))
-            player.y = (player.y + SCREEN_HEIGHT - 1) % SCREEN_HEIGHT;
-
-        if (key_held(KEY_DOWN))
-            player.y = (player.y + 1) % SCREEN_HEIGHT;
-
-        m3_plot(player.x, player.y, CLR_RED);
     }
 
     return 0;
